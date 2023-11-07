cc7f8f5489329ea767d6c70caa2e178d5c615ab6
Aashu-crypto <ashugandotra14@gmail.com>
2023-11-07 23:25:44 +0530

    projet started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomeScreen from '../screens/HomeScreens/HomeScreen'
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <HomeScreen/>
+    </>
+  )
+}
+
+export default App
