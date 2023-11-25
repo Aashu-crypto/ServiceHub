6eec9d66224e315ad09b4b042d7d38880ba9d6cc
Aashu-crypto <ashugandotra14@gmail.com>
2023-11-25 17:28:41 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import HomeScreen from '../screens/HomeScreens/HomeScreen'
+import React from "react";
+import HomeScreen from "../screens/HomeScreens/HomeScreen";
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <HomeScreen/>
+      <h1 className="text-3xl font-bold underline bg-slate-400">Hello world!</h1>
+      <HomeScreen />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
